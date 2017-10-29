e402172edcf042c858c22fd71f78100644d3097c
Vitor Felix <vytorfelix2@gmail.com>
2017-10-29 21:28:37 -0200

    hello world

---
+++ CirDesWithVHDL/1/full_adder.vhd
@@ -0,0 +1,12 @@
+ENTITY full_adder IS
+PORT(
+	a, b, cin: IN BIT;
+	s, cout: OUT BIT
+);
+END full_adder;
+
+ARCHITECTURE dataflow OF full_adder IS 
+BEGIN 
+	s <= a XOR b XOR cin;
+	cout <= (a AND b) OR (a AND cin) OR (b and cin);
+END dataflow;
